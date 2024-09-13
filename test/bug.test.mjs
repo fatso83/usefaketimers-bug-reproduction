54c812d9fc5e0ed391d8477f355f578ccb519b69
Carl-Erik Kopseng <carlerik@gmail.com>
2024-09-13 13:38:57 +0200

    show example of how to use runToLastAsync

--- test/bug.test.mjs
+++ test/bug.test.mjs
@@ -6,20 +6,28 @@ import { before, describe, it } from "mocha";
 import sinon from "sinon";
 
 describe("sinon bug reproduction", function () {
-  let result;
+  let result, clock;
 
-  before(async function () {
-    const clock = sinon.useFakeTimers({ now: 100 });
+  beforeEach(function () {
+    clock = sinon.useFakeTimers({ now: 100 });
+  });
+  afterEach(function () {
+    clock.restore();
+  });
+
+  async function setup() {
     const stream = Readable.from(Buffer.from("test"));
     const chunks = [];
     for await (const chunk of stream) {
       chunks.push(chunk);
     }
     result = Buffer.concat(chunks).toString("utf8");
-    clock.restore();
-  });
+  }
 
-  it("should run successfully", function () {
+  it("should run successfully", async function () {
+    const p = setup();
+    await clock.runToLastAsync();
+    await p;
     expect(result).to.equal("test");
   });
 });
